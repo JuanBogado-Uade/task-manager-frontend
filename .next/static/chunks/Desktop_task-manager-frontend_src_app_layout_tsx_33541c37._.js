8c8a8332b8afcc2a7ec18ce690dd368bbe4aea4e
Juan Bogado <jubogado@uade.edu.ar>
2025-10-05 19:19:40 -0300

    Delete .next directory

--- .next/static/chunks/Desktop_task-manager-frontend_src_app_layout_tsx_33541c37._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d21bc479._.css",
-  "static/chunks/Desktop_task-manager-frontend_32de5ccb._.js"
-],
-    source: "dynamic"
-});
